a85d6060db0974432ede1e2373034bd012b739d3
ritajarbanda <rjarbanda@students.avc.edu>
2023-11-18 04:09:00 +0000

    data and name

--- BrLw.R
+++ BrLw.R
@@ -1,9 +1,9 @@
-BrLw <-function(numClass,odds,countoDo){
-
-days=1:365
-count=countoDo
-repeatn=numClass
-varstor=numeric()
+BrLw <-function(numClass=35,odds=2,countoDo=637){
+  
+  days=1:365
+  count=countoDo
+  repeatn=numClass
+  varstor=numeric()
   brownlow = 0
   for (i in 1:countoDo) {
     sam=sample(days,size=numClass,replace = T)
@@ -11,5 +11,6 @@ varstor=numeric()
     if (sum(y)>0) brownlow=brownlow+1
     else brownlow = brownlow - odds
   }
- return(brownlow)
+  return(brownlow)
 }
+
